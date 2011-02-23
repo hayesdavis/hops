130d6538c33186ae35be12b3b6865ea0b9b388b1
Hayes Davis <hayes@appozite.com>
2011-02-22 19:01:35 -0800

    TextLine sugar.

--- src/java/cascading/hops/Sugar.java
+++ src/java/cascading/hops/Sugar.java
@@ -5,17 +5,21 @@ import cascading.tap.Tap;
 import cascading.tuple.Fields;
 
 public class Sugar {
-    
-    public static Fields $(String...fields) {
+
+    public static Fields $(String... fields) {
         return new Fields(fields);
     }
-    
+
     public static Tap tsv(Fields fields, String path) {
-        return Taps.hfs(Schemes.tsv(fields),path);
+        return Taps.hfs(Schemes.tsv(fields), path);
     }
-    
+
     public static Tap tsv(Fields fields, String path, SinkMode mode) {
-        return Taps.hfs(Schemes.tsv(fields),path, mode);
+        return Taps.hfs(Schemes.tsv(fields), path, mode);
+    }
+
+    public static Tap textLine(Fields fields, String path) {
+        return Taps.hfs(Schemes.textLine(fields), path);
     }
 
 }
