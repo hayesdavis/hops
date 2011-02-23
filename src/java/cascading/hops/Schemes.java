24b9ae76807b4d18a95440983b6193487cbe5e26
Hayes Davis <hayes@appozite.com>
2011-02-22 19:01:16 -0800

    Helper for TextLine scheme.

--- src/java/cascading/hops/Schemes.java
+++ src/java/cascading/hops/Schemes.java
@@ -2,6 +2,7 @@ package cascading.hops;
 
 import cascading.scheme.Scheme;
 import cascading.scheme.TextDelimited;
+import cascading.scheme.TextLine;
 import cascading.tuple.Fields;
 
 public class Schemes {
@@ -12,6 +13,10 @@ public class Schemes {
     
     public static Scheme csv(Fields fields) {
         return new TextDelimited(fields,",");
+    }
+    
+    public static Scheme textLine(Fields fields) {
+        return new TextLine(fields);
     }
     
 }
